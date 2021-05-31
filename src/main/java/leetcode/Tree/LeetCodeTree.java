0e8b1bccffce365403099360a539345051ddf381
dengqunjin <www.1146995251@qq.com>
2021-05-31 14:41:24 +0800

    二分查找：240

--- src/main/java/leetcode/Tree/LeetCodeTree.java
+++ src/main/java/leetcode/Tree/LeetCodeTree.java
@@ -1,6 +1,8 @@
 package leetcode.Tree;
 
 
+import leetcode.BinarySearch.BinarySearchSimple;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -313,7 +315,18 @@ public class LeetCodeTree {
         rightSideView(root.left,res,dept);
     }
 
-
+    //222. 完全二叉树的节点个数
+    //给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+    //完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，
+    /*并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。*/
+    public int countNodes(TreeNode root) {
+        if(null == root){
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return 1 + left + right;
+    }
 
     //872. 叶子相似的树
     //请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
