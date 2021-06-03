9c0c860d8d2e1c8b0a2ad2505d15ae21b4af5a56
dengqunjin <www.1146995251@qq.com>
2021-06-03 17:01:32 +0800

    排序题目：922，1491,1502,1387

--- src/main/java/leetcode/sorts/LeetCodeSort.java
+++ src/main/java/leetcode/sorts/LeetCodeSort.java
@@ -1,9 +1,6 @@
 package leetcode.sorts;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class LeetCodeSort {
     //56. 合并区间
@@ -45,6 +42,28 @@ public class LeetCodeSort {
         }
     }
 
+    //922. 按奇偶排序数组 II
+    //给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+    //对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+    public int[] sortArrayByParityII(int[] nums) {
+        int odd = 1;
+        int even = 0;
+        while (odd < nums.length && even < nums.length){
+            while (even < nums.length && nums[even] % 2 == 0){
+                even += 2;
+            }
+            while (odd < nums.length && nums[odd] % 2 == 1){
+                odd += 2;
+            }
+            if (odd < nums.length && even < nums.length){
+                nums[odd] ^= nums[even];
+                nums[even] ^= nums[odd];
+                nums[odd] ^= nums[even];
+            }
+        }
+        return nums;
+    }
+
     //1356. 根据数字二进制下 1 的数目排序
     //给你一个整数数组 arr 。请你将数组中的元素按照其二进制表示中数字 1 的数目升序排序。
     //如果存在多个数字二进制中 1 的数目相同，则必须将它们按照数值大小升序排列。
@@ -74,6 +93,124 @@ public class LeetCodeSort {
         arr[i] ^= arr[j];
         arr[j] ^= arr[i];
         arr[i] ^= arr[j];
+    }
+
+    //1491. 去掉最低工资和最高工资后的工资平均值
+    //给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。
+    //请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。
+    public double average(int[] salary) {
+        if (salary.length <= 2){
+            return 0;
+        }
+        int min = salary[0];
+        int max = salary[0];
+        int sum = salary[0];
+        for (int i = 1; i < salary.length; i++) {
+            sum += salary[i];
+            if (salary[i] > max){
+                max = salary[i];
+            }
+            if (salary[i] < min){
+                min = salary[i];
+            }
+        }
+        return (sum - max - min + 0.0) / (salary.length - 2);
+    }
+
+    //1502. 判断能否形成等差数列
+    //给你一个数字数组 arr 。
+    //如果一个数列中，任意相邻两项的差总等于同一个常数，那么这个数列就称为 等差数列 。
+    //如果可以重新排列数组形成等差数列，请返回 true ；否则，返回 false 。
+    public static boolean canMakeArithmeticProgression(int[] arr) {
+        if (arr.length <= 2){
+            return true;
+        }
+        sortQuick(arr,0,arr.length - 1);
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void sortQuick(int[] arr, int start, int end){
+        if (start > end){
+            return;
+        }
+        int i = start;
+        int j = end;
+        int temp = arr[start];
+        while (i != j){
+            while (temp <= arr[j] && i < j){
+                j --;
+            }
+            while (temp >= arr[i] && i < j){
+                i ++;
+            }
+
+            if (i < j){
+                arr[i] ^= arr[j];
+                arr[j] ^= arr[i];
+                arr[i] ^= arr[j];
+            }
+
+        }
+        arr[start] = arr[i];
+        arr[i] = temp;
+        sortQuick(arr,start,i - 1);
+        sortQuick(arr,i + 1, end);
+    }
+
+    //1387. 将整数按权重排序
+    //我们将整数 x 的 权重 定义为按照下述规则将 x 变成 1 所需要的步数：
+    //如果 x 是偶数，那么 x = x / 2
+    //如果 x 是奇数，那么 x = 3 * x + 1
+    //比方说，x=3 的权重为 7 。因为 3 需要 7 步变成 1 （3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1）。
+    //给你三个整数 lo， hi 和 k 。你的任务是将区间 [lo, hi] 之间的整数按照它们的权重 升序排序 ，如果大于等于 2 个整数有 相同 的权重，那么按照数字自身的数值 升序排序 。
+    //请你返回区间 [lo, hi] 之间的整数按权重排序后的第 k 个数。
+    public static int getKth(int lo, int hi, int k) {
+        if (k > hi - lo + 1){
+            return -1;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        int[][] res = new int[hi - lo + 1][2];
+        int index = 0;
+        for (int i = lo; i <= hi; i++) {
+            res[index ++ ] = new int[]{i,getSteps(map,i)};
+        }
+        Arrays.sort(res, (a, b) -> {
+            if(a[1] == b[1]){
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+        return res[k - 1][0];
+    }
+
+    public static int getSteps(Map<Integer, Integer> map, int i){
+        if (map.containsKey(i)){
+            return map.get(i);
+        }
+        List<Integer> list = new ArrayList();
+        list.add(i);
+        while (i != 1){
+            if (i % 2 == 0){
+                i = i >> 1;
+            }else {
+                i = 3 * i + 1;
+            }
+            if (map.containsKey(i)){
+                return map.get(i) + list.size();
+            }
+            list.add(i);
+        }
+
+        for (int j = 0; j < list.size(); j++) {
+            map.put(list.get(j),list.size() - j - 1);
+        }
+        return list.size() - 1;
     }
 
 
