911644802bc3d2093c4ee3701c259c513b9510c6
dengqunjin <www.1146995251@qq.com>
2021-06-03 14:20:48 +0800

    前缀和：525

--- src/main/java/leetcode/sumPrefix/LeetCodeSumPrefix.java
+++ src/main/java/leetcode/sumPrefix/LeetCodeSumPrefix.java
@@ -32,7 +32,7 @@ public class LeetCodeSumPrefix {
     //子数组元素总和为 k 的倍数。
     //如果存在，返回 true ；否则，返回 false 。
     //如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
-    public boolean checkSubarraySum2(int[] nums, int k) {
+    public static boolean checkSubarraySum2(int[] nums, int k) {
         int m = nums.length;
         if (m < 2) {
             return false;
@@ -52,5 +52,36 @@ public class LeetCodeSumPrefix {
             }
         }
         return false;
+    }
+
+    //525. 连续数组
+    //给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
+    public static int findMaxLength(int[] nums) {
+        if (nums.length < 2){
+            return 0;
+        }
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int count = 0;
+        map.put(0,-1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1){
+                count ++ ;
+            }
+            if (nums[i] == 0){
+                count -- ;
+            }
+            if (map.containsKey(count)){
+                Integer preIndex = map.get(count);
+                res = Math.max(res,i - preIndex);
+            }else {
+                map.put(count,i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        findMaxLength(new int[]{1,0});
     }
 }
