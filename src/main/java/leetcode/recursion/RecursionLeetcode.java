decd3d5c45530598adc82a9663647751cdfe341a
dengqunjin <www.1146995251@qq.com>
2021-06-07 10:54:19 +0800

    动态规划和回溯：494

--- src/main/java/leetcode/recursion/RecursionLeetcode.java
+++ src/main/java/leetcode/recursion/RecursionLeetcode.java
@@ -358,6 +358,65 @@ public class RecursionLeetcode {
         return count;
     }
 
+    //494. 目标和
+    //给你一个整数数组 nums 和一个整数 target 。
+    //向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+    //例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+    //返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+    public static int findTargetSumWays(int[] nums, int target) {
+        int[][] res = {{0}};
+        findTargetSumWays(res,nums,target,0,0);
+        return res[0][0];
+    }
+
+    public static void findTargetSumWays(int[][] res, int[] nums, int target, int index, int sum) {
+        if (index == nums.length){
+            if (sum == target){
+                res[0][0] = res[0][0] + 1;
+            }
+
+            return;
+        }
+        sum += nums[index];
+        findTargetSumWays(res,nums,target,index + 1,sum);
+        sum = sum - 2 * nums[index];
+        findTargetSumWays(res,nums,target,index + 1,sum);
+    }
+
+    //大佬的  动态规划
+    //494. 目标和
+    //给你一个整数数组 nums 和一个整数 target 。
+    //向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+    //例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+    //返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+    public int findTargetSumWays2(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int n = nums.length, neg = diff / 2;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
+    }
+
+    public static void main(String[] args) {
+        findTargetSumWays(new int[]{1,1,1,1,1},3);
+    }
+
     //784. 字母大小写全排列
     //给定一个字符串S，通过将字符串S中的每个字母转变大小写，我们可以获得一个新的字符串。返回所有可能得到的字符串集合。
     public static List<String> letterCasePermutation(String s) {
