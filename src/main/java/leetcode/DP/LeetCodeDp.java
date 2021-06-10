5e974876a704795d46f7eaa631f47d0090d16ba0
dengqunjin <www.1146995251@qq.com>
2021-06-10 10:39:11 +0800

    动态规划:518

--- src/main/java/leetcode/DP/LeetCodeDp.java
+++ src/main/java/leetcode/DP/LeetCodeDp.java
@@ -3,6 +3,7 @@ package leetcode.DP;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LeetCodeDp {
@@ -61,5 +62,42 @@ public class LeetCodeDp {
             }
         }
         return dp[0];
+    }
+
+    //198. 打家劫舍
+    //你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+    //如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+    //给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+    public int rob(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1){
+            return nums[0];
+        }
+        if (nums.length == 2){
+            return Math.max(nums[0],nums[1]);
+        }
+        int first = nums[0];
+        int secend = Math.max(nums[0],nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int temp = Math.max(nums[i] + first,secend);
+            first = secend;
+            secend = temp;
+        }
+        return secend;
+    }
+
+    //518. 零钱兑换 II
+    //给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。SSS
+    public static int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
     }
 }
