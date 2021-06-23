e747be9e08ff2cdf2bd2422aabc112e1ee0334fa
dengqunjin <www.1146995251@qq.com>
2021-06-23 16:31:58 +0800

    LeetcodeMath223，319

--- src/main/java/leetcode/maths/MathLeetCode.java
+++ src/main/java/leetcode/maths/MathLeetCode.java
@@ -45,6 +45,68 @@ public class MathLeetCode {
         return result == x;
     }
 
+    //223. 矩形面积
+    //给你 二维 平面上两个 由直线构成的 矩形，请你计算并返回两个矩形覆盖的总面积。
+    //每个矩形由其 左下 顶点和 右上 顶点坐标表示：
+    //第一个矩形由其左下顶点 (ax1, ay1) 和右上顶点 (ax2, ay2) 定义。
+    //第二个矩形由其左下顶点 (bx1, by1) 和右上顶点 (bx2, by2) 定义。
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+        if (bx1 >= ax2 || ax1 >= bx2 || ay1 >= by2 || by1 >= ay2){
+            return area1 + area2;
+        }
+        int zx1,zy1,zx2,zy2;
+        zx1 = Math.max(ax1, bx1);
+
+        zx2 = Math.min(ax2, bx2);
+
+        zy1 = Math.max(ay1, by1);
+
+        zy2 = Math.min(ay2, by2);
+
+        return area1 + area2 - (zx2 - zx1) * (zy2 - zy1);
+    }
+
+    //超时
+    //319. 灯泡开关
+    //初始时有 n 个灯泡处于关闭状态。
+    //对某个灯泡切换开关意味着：如果灯泡状态为关闭，那该灯泡就会被开启；而灯泡状态为开启，那该灯泡就会被关闭。
+    //第 1 轮，每个灯泡切换一次开关。即，打开所有的灯泡。
+    //第 2 轮，每两个灯泡切换一次开关。 即，每两个灯泡关闭一个。
+    //第 3 轮，每三个灯泡切换一次开关。
+    //第 i 轮，每 i 个灯泡切换一次开关。 而第 n 轮，你只切换最后一个灯泡的开关。
+    //找出 n 轮后有多少个亮着的灯泡。
+    public static int bulbSwitch(int n) {
+        boolean[] flag = new boolean[n + 1];
+        for (int step = 1; step <= n; step++) {
+            for (int i = 1; step * i <= n; i++) {
+                flag[step * i] = !flag[step * i];
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < flag.length; i++) {
+            if (flag[i]){
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    //大佬的
+    //319. 灯泡开关
+    //初始时有 n 个灯泡处于关闭状态。
+    //对某个灯泡切换开关意味着：如果灯泡状态为关闭，那该灯泡就会被开启；而灯泡状态为开启，那该灯泡就会被关闭。
+    //第 1 轮，每个灯泡切换一次开关。即，打开所有的灯泡。
+    //第 2 轮，每两个灯泡切换一次开关。 即，每两个灯泡关闭一个。
+    //第 3 轮，每三个灯泡切换一次开关。
+    //第 i 轮，每 i 个灯泡切换一次开关。 而第 n 轮，你只切换最后一个灯泡的开关。
+    //找出 n 轮后有多少个亮着的灯泡。
+    public static int bulbSwitch2(int n) {
+        return (int)Math.floor(Math.sqrt(n));
+    }
+
+
     //367. 有效的完全平方数
     //给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
     //进阶：不要 使用任何内置的库函数，如  sqrt 。
@@ -80,8 +142,4 @@ public class MathLeetCode {
         return (x * x == num);
     }
 
-    public static void main(String[] args) {
-        System.out.println(Math.sqrt(808201));
-        isPerfectSquare(808201);
-    }
 }
