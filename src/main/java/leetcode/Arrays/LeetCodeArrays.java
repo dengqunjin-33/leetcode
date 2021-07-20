1d83ea07e1f8657c45d932217724e8cc3e8f608e
dengqunjin <www.1146995251@qq.com>
2021-07-20 16:49:54 +0800

    数组：1877

--- src/main/java/leetcode/Arrays/LeetCodeArrays.java
+++ src/main/java/leetcode/Arrays/LeetCodeArrays.java
@@ -1168,4 +1168,22 @@ public class LeetCodeArrays {
         }
         return count;
     }
+
+    //1877. 数组中最大数对和的最小值
+    //一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+    //比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+    //给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+    //nums 中每个元素 恰好 在 一个 数对中，且
+    //最大数对和 的值 最小 。
+    //请你在最优数对划分的方案下，返回最小的 最大数对和 。
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        int times = nums.length >> 1;
+        int len = nums.length;
+        for (int i = 0; i < times; i++) {
+            res = Math.max(res,nums[i] + nums[len - 1 - i]);
+        }
+        return res;
+    }
 }
