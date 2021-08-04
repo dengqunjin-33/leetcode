b8cd0f96296f615dab2f768cebea0ab26b73e8e7
dengqunjin <www.1146995251@qq.com>
2021-08-04 15:46:31 +0800

    数组：611

--- src/main/java/leetcode/Arrays/LeetCodeArrays600later.java
+++ src/main/java/leetcode/Arrays/LeetCodeArrays600later.java
@@ -4,6 +4,58 @@ import java.util.*;
 
 public class LeetCodeArrays600later {
 
+    //排序 + 暴力
+    //执行用时：1281 ms, 在所有 Java 提交中击败了11.21%的用户
+    //内存消耗：38.3 MB, 在所有 Java 提交中击败了13.27%的用户
+    //611. 有效三角形的个数
+    //给定一个包含非负整数的数组，你的任务是统计其中可以组成三角形三条边的三元组个数。
+    public int triangleNumber(int[] nums) {
+        if (nums.length < 3){
+            return 0;
+        }
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length -1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] <= nums[k]){
+                        break;
+                    }
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    //排序 + 双指针
+    //执行用时：30 ms, 在所有 Java 提交中击败了91.22%的用户
+    //内存消耗：38.2 MB, 在所有 Java 提交中击败了18.54%的用户
+    //611. 有效三角形的个数
+    //给定一个包含非负整数的数组，你的任务是统计其中可以组成三角形三条边的三元组个数。
+    public int triangleNumber2(int[] nums) {
+        int len = nums.length;
+        if (len < 3){
+            return 0;
+        }
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = len - 1; i >= 2; i--) {
+            int left = 0;
+            int right = i - 1;
+            while (left < right){
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+        return count;
+    }
+
+
     //645. 错误的集合
     //集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
     //给定一个数组 nums 代表了集合 S 发生错误后的结果。
