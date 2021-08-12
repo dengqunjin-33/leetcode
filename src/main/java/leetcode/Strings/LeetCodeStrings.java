357d17f6b88aa408065b2bbe305cfa7d8757ede5
dengqunjin <www.1146995251@qq.com>
2021-08-12 17:54:47 +0800

    String：516

--- src/main/java/leetcode/Strings/LeetCodeStrings.java
+++ src/main/java/leetcode/Strings/LeetCodeStrings.java
@@ -534,6 +534,27 @@ public class LeetCodeStrings {
         return sb.toString();
     }
 
+    //516. 最长回文子序列
+    //给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+    //子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            char c1 = s.charAt(i);
+            for (int j = i + 1; j < n; j++) {
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
     //726. 原子的数量
     //给定一个化学式formula（作为字符串），返回每种原子的数量。
     //原子总是以一个大写字母开始，接着跟随0个或任意个小写字母，表示原子的名字。
@@ -568,7 +589,7 @@ public class LeetCodeStrings {
             }
         }
 
-        StringBuffer res = new StringBuffer();
+        StringBuilder res = new StringBuilder();
         for (Map.Entry<String,Integer> entry : map.entrySet()) {
             res.append(entry.getKey());
             if (1 < entry.getValue()){
