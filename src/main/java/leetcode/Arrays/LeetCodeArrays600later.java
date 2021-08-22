1dd80e2177f9b3497747d71eb847bea0025dab27
dengqunjin <www.1146995251@qq.com>
2021-08-22 14:16:30 +0800

    数组：789

--- src/main/java/leetcode/Arrays/LeetCodeArrays600later.java
+++ src/main/java/leetcode/Arrays/LeetCodeArrays600later.java
@@ -117,6 +117,25 @@ public class LeetCodeArrays600later {
         return res;
     }
 
+    //执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    //内存消耗：38.1 MB, 在所有 Java 提交中击败了44.71%的用户
+    //789. 逃脱阻碍者
+    //你在进行一个简化版的吃豆人游戏。你从 [0, 0] 点开始出发，你的目的地是 target = [xtarget, ytarget] 。地图上有一些阻碍者，以数组 ghosts 给出，第 i 个阻碍者从 ghosts[i] = [xi, yi] 出发。所有输入均为 整数坐标 。
+    //每一回合，你和阻碍者们可以同时向东，西，南，北四个方向移动，每次可以移动到距离原位置 1 个单位 的新位置。当然，也可以选择 不动 。所有动作 同时 发生。
+    //如果你可以在任何阻碍者抓住你 之前 到达目的地（阻碍者可以采取任意行动方式），则被视为逃脱成功。如果你和阻碍者同时到达了一个位置（包括目的地）都不算是逃脱成功。
+    //只有在你有可能成功逃脱时，输出 true ；否则，输出 false 。
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        // 计算玩家和鬼谁先到达终点
+        int playerDistance = Math.abs(target[0]) + Math.abs(target[1]);
+        for (int[] ghost : ghosts) {
+            int ghostDistance = Math.abs(ghost[0] - target[0]) + Math.abs(ghost[1] - target[1]);
+            if (ghostDistance <= playerDistance) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     //超时了
     //802. 找到最终的安全状态
     //在有向图中，以某个节点为起始节点，从该点出发，每一步沿着图中的一条有向边行走。如果到达的节点是终点（即它没有连出的有向边），则停止。
