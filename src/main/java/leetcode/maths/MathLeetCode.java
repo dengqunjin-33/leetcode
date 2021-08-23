eec0d0c5298da0dceedd1628ee011cf564e4d19f
dengqunjin <www.1146995251@qq.com>
2021-08-23 14:56:59 +0800

    数学：1646

--- src/main/java/leetcode/maths/MathLeetCode.java
+++ src/main/java/leetcode/maths/MathLeetCode.java
@@ -578,5 +578,35 @@ public class MathLeetCode {
         return Math.min(c,360 - c);
     }
 
+    //执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    //内存消耗：35.4 MB, 在所有 Java 提交中击败了15.03%的用户
+    //1646. 获取生成数组中的最大值
+    //给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+    //nums[0] = 0
+    //nums[1] = 1
+    //当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+    //当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+    //返回生成数组 nums 中的 最大 值。
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for (int i = 0; i < n; i++) {
+            if (2 * i <= n) {
+                nums[2 * i] = nums[i];
+            }
+            if (2 * i + 1 <= n) {
+                nums[2 * i + 1] = nums[i] + nums[i + 1];
+            }
+        }
+        int ans = 0;
+        for (int i : nums) {
+            ans = Math.max(ans, i);
+        }
+        return ans;
+    }
 
 }
